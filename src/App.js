006aa3899c9d91287787fb2a2d57e977d0967bf0
Stimp24 <stinson.prod@gmail.com>
2018-12-30 12:50:06 -0500

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
@@ -20,6 +20,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Button bsStyle='success'>Hello</Button>
       </div>
     );
   }
