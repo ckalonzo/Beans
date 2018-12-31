2b2d963de1be77f4baa56fb5c90a4f3b448d23ea
Stimp24 <stinson.prod@gmail.com>
2018-12-31 02:39:27 -0500

    adding animation to navbar

--- src/Pages/100_Home/component/Navbar.jsx
+++ src/Pages/100_Home/component/Navbar.jsx
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 import NavbarLinks from '../content/Navbar';
-
+//import { moveLine } from '../js/Moveline'
 
 export default class Navbar extends Component {
     render() {
         const { content } = this.props;
         return (
             <div>
-                <div className="container">
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-                        <Link className="navbar-brand" to="/"> < img src="../img/truckedaway-wh-bg.jpg" width="30" height="30" alt="" ></img></Link>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarCollapse">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/Membership">Membership</Link>
-                                </li>
+                <nav class="navbar navbar-expand-md navbar-dark bg-light hover-underline-menu">
+                    <a class="navbar-brand" to="/Home"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse" id="collapsingNavbar">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link moveline dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Customer</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown08">
+                                    <a class="dropdown-item moveline" href="/SignUp">Sign Up</a>
+                                    <a class="dropdown-item moveline" href="/Login">LogIn</a>
 
-                            </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link moveline" to="/Membership">Membership</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link moveline dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contractor</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown08">
+                                    <a class="dropdown-item moveline" href="/SignUp">Sign Up</a>
+                                    <a class="dropdown-item moveline" href="/Login">LogIn</a>
+                                </div>
+                            </li>
+                        </ul>
 
-                        </div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
 
             </div>
         );
