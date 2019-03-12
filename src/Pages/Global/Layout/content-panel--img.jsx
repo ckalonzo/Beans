123753519ce1b75964af61af2ee8b2b0ea22ebb9
Stimp24 <stinson.prod@gmail.com>
2019-03-11 22:28:21 -0400

    css changes

--- src/Pages/Global/Layout/content-panel--img.jsx
+++ src/Pages/Global/Layout/content-panel--img.jsx
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import './Css/content-panel--img.css';
+import './Css/content-panel--img.scss';
 
 export default class ContentPanelImg extends Component {
     render() {
 
         return (
-            <section>
+            <section className="wwo mb-5">
                 <div className="container">
                     <div className="row">
-                        <div className="col-6 col-md-6 col-sm-12">
+                        <div className="col-lg-6 col-md-12 col-sm-12">
                             <div className="img-panel">
                             </div>
                         </div>
-                        <div className="col-6 col-md-6 col-sm-12">
+                        <div className="col-lg-6 col-md-12 col-sm-12">
                             <div className="mt-5">
-                                <div className="">
+                                <div className="col-md-12 col-sm-12">
                                     <h2 className="img-content-title">What we offer</h2>
                                 </div>
                                 <div className="mb-3 img-content-sub-title">
                                     <h5>We offer the best and simple solution to have your service done when you want it </h5>
                                 </div>
                                 <div className="img-content-copy">
-                                    <ul>
+                                    <ul className="no--bullets">
                                         <li>
                                             <p>Morbi pulvinar massa vitae sem cursus ferlentu</p>
                                         </li>
