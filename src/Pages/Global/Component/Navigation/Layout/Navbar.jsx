4ad1a1af23e20b3149aa83f7ea584b79615cd1d4
Brandon <stinson.prod@gmail.com>
2019-05-23 20:29:07 -0400

    fix: single bid card

--- src/Pages/Global/Component/Navigation/Layout/Navbar.jsx
+++ src/Pages/Global/Component/Navigation/Layout/Navbar.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import ProviderSignedInLinks from "./ProviderSignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
-import { connect } from 'react-redux'
-import { MDBBtn } from "mdbreact";
+import { connect } from "react-redux";
 
-const Navbar = (props) => {
-    const { auth, profile } = props;
-    // display links if user is signed in or out
-    const links = auth.uid ? <ProviderSignedInLinks profile={profile} /> : <SignedOutLinks />;
-    return (
+const Navbar = props => {
+  const { auth, profile } = props;
+  // display links if user is signed in or out
+  const links = auth.uid ? (
+    <ProviderSignedInLinks profile={profile} />
+  ) : (
+    <SignedOutLinks />
+  );
+  return (
+    <nav class="navbar navbar-light sticky-top navbar-expand-md bg-faded justify-content-center nav-section hover-underline-menu">
+      <div className="container">
+        <Link to="/" class="navbar-brand d-flex w-50 mr-auto" />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsingNavbar3"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+          {links}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-        <nav class="navbar navbar-light sticky-top navbar-expand-md bg-faded justify-content-center nav-section hover-underline-menu">
-            <div className="container">
-                <Link
-                    to="/"
-                    class="navbar-brand d-flex w-50 mr-auto">
-                </Link>
-                <MDBBtn class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
-                    <span class="navbar-toggler-icon"></span>
-                </MDBBtn>
-                <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
-                    {links}
-                </div>
-            </div>
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  };
+};
 
-        </nav>
-
-
-
-    )
-}
-
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth,
-        profile: state.firebase.profile
-    }
-}
-
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
