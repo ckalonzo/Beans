dec85e308391f8d45a6b8249ce264c350cfcd771
Stimp24 <stinson.prod@gmail.com>
2019-05-30 21:37:12 -0400

    making changes to radio button

--- src/Pages/106_JobPosts/Component/AdditionalInfo/FlightOfStairs/FlightOfStairs.jsx
+++ src/Pages/106_JobPosts/Component/AdditionalInfo/FlightOfStairs/FlightOfStairs.jsx
@@ -1,92 +1,99 @@
 import React, { Component } from "react";
 import "../../../Css/PostJob.scss";
-import { MDBFormInline, MDBInput } from "mdbreact";
 
 class FlightOfStairs extends Component {
-  state = {
-    radio: 6
-  };
-
-  onClick = nr => () => {
-    this.setState({
-      radio: nr
-    });
-  };
   render() {
     const { handleChange, flightOfStairs, values } = this.props;
     return (
-      <MDBFormInline className="mt-5">
-        <MDBInput
-          gap
-          onClick={this.onClick(1)}
-          checked={this.state.radio === 1 ? true : false}
-          label="0"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(2)}
-          checked={this.state.radio === 2 ? true : false}
-          label="1"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(3)}
-          checked={this.state.radio === 3 ? true : false}
-          label="2"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(4)}
-          checked={this.state.radio === 4 ? true : false}
-          label="3"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(5)}
-          checked={this.state.radio === 5 ? true : false}
-          label="4"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(6)}
-          checked={this.state.radio === 6 ? true : false}
-          label="5"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-        <MDBInput
-          gap
-          onClick={this.onClick(7)}
-          checked={this.state.radio === 7 ? true : false}
-          label="6"
-          type="radio"
-          id={flightOfStairs}
-          value={values.largeItems}
-          onChange={handleChange("flightOfStairs")}
-        />
-      </MDBFormInline>
+      <div className="input-group mt-5">
+        <div class="form-check form-check-inline">
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                checked={true}
+                id="iuiuipo"
+                onChange={handleChange("flightOfStairs")}
+                value={values.largeItems}
+              />
+              1
+            </label>
+          </div>
+          {/* <input
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            id={flightOfStairs}
+            //value="option1"
+            onChange={handleChange("flightOfStairs")}
+            value={values.largeItems}
+          />
+          <label class="form-check-label" for="inlineRadio1">
+            1
+          </label>*/}
+        </div>
+        <div class="form-check form-check-inline">
+          <div className="radio">
+            <label>
+              <input
+                type="radio"
+                id="hghgh"
+                onChange={handleChange("flightOfStairs")}
+                value={values.largeItems}
+              />
+              2
+            </label>
+          </div>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id={flightOfStairs}
+            value={values.largeItems}
+            onChange={handleChange("flightOfStairs")}
+          />
+          <label className="form-check-label" for="flightofStairs3">
+            3
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id={flightOfStairs}
+            value={values.largeItems}
+            onChange={handleChange("flightOfStairs")}
+          />
+          <label className="form-check-label" for="flightofStairs4">
+            4
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id={flightOfStairs}
+            value={values.largeItems}
+            onChange={handleChange("flightOfStairs")}
+          />
+          <label className="form-check-label" for="flightofStairs5">
+            5
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id={flightOfStairs}
+            value={values.largeItems}
+            onChange={handleChange("flightOfStairs")}
+          />
+          <label className="form-check-label" for="flightofStairs6">
+            6
+          </label>
+        </div>
+      </div>
     );
   }
 }
