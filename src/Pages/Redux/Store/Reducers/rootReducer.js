39a9d39d35c13c11874a904b19bc5a4ba8e1dc92
Brandon <stinson.prod@gmail.com>
2019-07-13 01:27:56 -0400

    Responsive

--- src/Pages/Redux/Store/Reducers/rootReducer.js
+++ src/Pages/Redux/Store/Reducers/rootReducer.js
@@ -2,7 +2,7 @@ import authReducer from "./authReducer";
 import JobPostReducer from "./JobPostReducer";
 import cartReducer from "./cartReducer";
 import CounterReducer from "./counterReducer";
-import monthlySubscriptionReducer from "./monthlyMembership";
+import monthlySubscriptionReducer from "./monthlyMembershipReducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
 import { firebaseReducer } from "react-redux-firebase";
