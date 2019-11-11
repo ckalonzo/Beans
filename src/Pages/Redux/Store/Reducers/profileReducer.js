efb9c97dd3844a9e68ca3471c49617b18fe3fe96
Stimp24 <stinson.prod@gmail.com>
2019-11-11 06:17:28 -0500

    WIP Updating profile

--- src/Pages/Redux/Store/Reducers/profileReducer.js
+++ src/Pages/Redux/Store/Reducers/profileReducer.js
@@ -1,32 +1,44 @@
 import * as actionTypes from "../actions/actionTypes/ta-actionTypes";
 
 const initState = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  phone: "",
-  password: "",
-  confirmPassword: "",
-  address1: "",
-  address2: "",
-  city: "Greenville",
-  state: "AZ",
-  zip: "30336",
-  rating: "4.5",
-  reviews: [{ title: "Testing 123", text: "Brandon Was great!!!" }],
-  skills: [],
-  activeMember: "",
-  bio:
-    "'The Warriors have far too much firepower for the Los Angeles Clippers to overcome in their first-round NBA playoff series.Everyone knows it.It was evident Saturday night at Oracle Arena when the two-time defending champions didnt have nearly their A-game but still ended up cruising to a win behind Steph Currys 38-point barrage.To anyone who watched Game 1, its clear the series appears to be headed for a sweep as the Clippers just dont possess the talent needed to overthrow the NBAs king. Game was a hotly contested, physical game that featured a number of the Clippers receiving technical fouls, and Kevin Durant and Patrick Beverley being ejected in the fourth quarter. Getting under the Warriors skin, the way Beverley did to Durant, is the only way the Clippers will have a chance in the series, according to Warriors assistant coach Mike Brown. We dont want to take the bait, Brown said on SNC: Warriors Playoff Central.Theres no way Id ever want to trade Kevin Durant for Patrick Beverley. We love the guys that we have on this team, and so one of our keys heading into this series was, we know we have to be poised against this team. They have no other way to beat us unless they junk it up, and they take advantage of us either getting kicked out or doing something silly to make it an advantage for them. After the game, Durant vowed to keep his poise during the remainder of the series, while also showing respect to Beverley and the grit that he brings for the Clippers.',",
-  numberOfJobs: "3000",
-  typeOfJobs: ["LawnCare", "HouseCleaning", "JunkRemoval"],
-  years: "10",
-  bkcheck: false,
-  basicinfochk: false,
-  companyName: "Lucy Goosie",
-  history: "",
-  bids: "",
-  update: false
+  userData: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+    address1: "",
+    address2: "",
+    city: "Greenville",
+    state: "AZ",
+    zip: "30336",
+    rating: "4.5",
+    reviews: [{ title: "Testing 123", text: "Brandon Was great!!!" }],
+    skills: [],
+    activeMember: "",
+    bio:
+      "'The Warriors have far too much firepower for the Los Angeles Clippers to overcome in their first-round NBA playoff series.Everyone knows it.It was evident Saturday night at Oracle Arena when the two-time defending champions didnt have nearly their A-game but still ended up cruising to a win behind Steph Currys 38-point barrage.To anyone who watched Game 1, its clear the series appears to be headed for a sweep as the Clippers just dont possess the talent needed to overthrow the NBAs king. Game was a hotly contested, physical game that featured a number of the Clippers receiving technical fouls, and Kevin Durant and Patrick Beverley being ejected in the fourth quarter. Getting under the Warriors skin, the way Beverley did to Durant, is the only way the Clippers will have a chance in the series, according to Warriors assistant coach Mike Brown. We dont want to take the bait, Brown said on SNC: Warriors Playoff Central.Theres no way Id ever want to trade Kevin Durant for Patrick Beverley. We love the guys that we have on this team, and so one of our keys heading into this series was, we know we have to be poised against this team. They have no other way to beat us unless they junk it up, and they take advantage of us either getting kicked out or doing something silly to make it an advantage for them. After the game, Durant vowed to keep his poise during the remainder of the series, while also showing respect to Beverley and the grit that he brings for the Clippers.',",
+    numberOfJobs: "3000",
+    typeOfJobs: ["LawnCare", "HouseCleaning", "JunkRemoval"],
+    years: "10",
+    bkcheck: false,
+    basicinfochk: false,
+    companyName: "Lucy Goosie",
+    history: "",
+    bids: "",
+    update: false,
+    services: [
+      { value: "Junk_Removal", label: "Junk Removal" },
+      { value: "Delivery", label: "Delivery" },
+      { value: "Handy_Man", label: "Handy Man" },
+      { value: "Cleaning_Service", label: "Cleaning Service" }
+    ]
+  },
+  profile: "",
+  validForm: false,
+  errors: [],
+  errorMessage: {}
 };
 
 const ProfileReducer = (state = initState, action) => {
@@ -39,9 +51,20 @@ const ProfileReducer = (state = initState, action) => {
     case actionTypes.CREATE_PROFILE_SUCCESS:
       const createProfileSuccess = action.createProfile;
       return {
-        ...state,
-        [createProfileSuccess.firstName]: createProfileSuccess.value
+        ...state
       };
+    case actionTypes.UPDATE_PROFILE_START:
+      return {};
+    case actionTypes.UPDATE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        userData: {
+          ...state.userData
+          // [action.userDataCollection]: state.userData[action.userDataCollection]
+        }
+      };
+    case actionTypes.UPDATE_PROFILE_ERR:
+      return {};
     default:
       return state;
   }
--- src/Pages/Redux/Store/actions/profileAuctions.jsx
+++ src/Pages/Redux/Store/actions/profileAuctions.jsx
@@ -1,3 +1,5 @@
+import { UPDATE_PROFILE_SUCCESS } from "./actionTypes/ta-actionTypes";
+
 export const createProfile = (uid, userdata) => {
   // console.log(uid, userData);
   return (dispatch, getState, { getFirebase, getFirestore }) => {
@@ -29,30 +31,39 @@ export const createProfile = (uid, userdata) => {
 };
 
 export const updateProfile = (uid, userData) => {
+  console.log(uid, userData);
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firestore = getFirestore();
+    //console.log(firestore)
     firestore
       .collection("contractorProfile")
       .doc(uid)
       .set({
-        // firstName: userData.firstName,
-        // lastName: userData.lastName,
-        // initials: userData.firstName[0] + userData.lastName[0],
-        // email: userData.email,
-        // phone: userData.phone,
-        // address1: userData.address1,
-        // address2: userData.address2,
-        // city: userData.city,
-        // state: userData.state,
-        // zip: userData.zip,
-        // bio: userdata.bio,
-        // companyName: userdata.companyName
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        initials: userData.firstName[0] + userData.lastName[0],
+        email: userData.email,
+        phone: userData.phone,
+        address1: userData.address1,
+        address2: userData.address2,
+        city: userData.city,
+        state: userData.state,
+        zip: userData.zip,
+        bio: userData.bio,
+        companyName: userData.companyName
       })
-      .then(() => {
-        dispatch({ type: "CREATE_PROFILE_SUCCESS" });
+      .then(res => {
+        console.log(res);
+        dispatch({ type: "UPDATE_PROFILE_SUCCESS" });
       })
       .catch(err => {
-        dispatch({ type: "CREATE_PROFILE_ERROR", err });
+        dispatch({ type: "UPDATE_PROFILE_ERROR", err });
       });
+  };
+};
+export const userDataCollection = userDataCollection => {
+  return {
+    type: UPDATE_PROFILE_SUCCESS,
+    payload: userDataCollection
   };
 };
