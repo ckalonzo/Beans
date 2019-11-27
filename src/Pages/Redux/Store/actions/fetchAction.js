d09833010460ff7d930555fa7ac97706c3cfd3f1
Brandon <stinson.prod@gmail.com>
2019-11-27 18:39:32 -0500

    adding fetch action

---
+++ src/Pages/Redux/Store/actions/fetchAction.js
@@ -0,0 +1,20 @@
+export const fetchCustomerProfile = () => {
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+
+    return firestore
+      .collection("users")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          let items = doc.data();
+
+          /* Make data suitable for rendering */
+          items = JSON.stringify(items);
+          dispatch({ type: ACTION.FETCH_USER_SUCCESS, payload: items });
+          /* Update the components state with query result */
+          // this.setState({ items : items })
+        });
+      });
+  };
+};
